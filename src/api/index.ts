a5fc9bcd39b28e9c3aba19e7e07b7f1569834f9c
Annu <113963072+Anu1121@users.noreply.github.com>
2025-02-22 22:06:44 +0800

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost';
+const BASE_URL = 'http://18.140.234.198:8087';
 
 export const getData = async () => {
 	const b = await axios.get(`${BASE_URL}/api/data`);
